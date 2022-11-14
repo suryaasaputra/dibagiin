891d4df6d4f9078c408896b1e0da38f058948a33
arifrizqi <arifrizqy.ar15@gmail.com>
2022-11-14 12:56:11 +0700

    Initial commit

---
+++ src/scripts/routes/routes.js
@@ -0,0 +1,12 @@
+import Home from '../views/pages/home';
+import Login from '../views/pages/login';
+import Daftar from '../views/pages/register';
+
+const routes = {
+  '/': Home,
+  '/home': Home,
+  '/login': Login,
+  '/daftar': Daftar,
+};
+
+export default routes;
