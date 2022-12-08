41a90b6640e2b0d6c105cc45d0c6082c92e4bbab
suryaasaputra <suryaaasaputra.s@gmail.com>
2022-12-08 23:11:19 +0700

    fix bug rfetch-wrapper

--- helpers/fetch-wrapper.js
+++ helpers/fetch-wrapper.js
@@ -105,7 +105,7 @@ function handleResponse(response) {
 		const data = text && JSON.parse(text);
 
 		if (!response.ok) {
-			if ([401, 403].includes(response.status)) { //&& userService.userValue) {
+			if ([401, 403].includes(response.status) && userService.userValue) {
 				Swal.fire({
 					icon: "error",
 					title: `Sesi Anda telah berakhir, silahkan masuk kembali`,
