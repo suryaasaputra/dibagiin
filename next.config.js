338dcf3ba58fee6ed4431c579a3c945b26f04819
suryaasaputra <suryaaasaputra.s@gmail.com>
2022-12-09 11:29:12 +0700

    tes lagi

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
   dest: "public",
+  register: true,
+  skipWaiting: true,
   disable: process.env.NODE_ENV === "development"
 });
 const nextConfig = withPWA({
