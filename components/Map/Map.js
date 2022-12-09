dcfd5275774c1fbd43821e5d5069ecb4efed8858
suryaasaputra <suryaaasaputra.s@gmail.com>
2022-12-09 15:16:44 +0700

    fix typo

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -16,7 +16,7 @@ const Map = ({ children, className, ...rest }) => {
     L.Icon.Default.mergeOptions({
         iconRetinaUrl: "/images/marker-icon-2x.png'",
         iconUrl: '/images/marker-icon.png',
-        shadowUrl: '/images/marker - shadow.png',
+        shadowUrl: '/images/marker-shadow.png',
     });
     return (
         <MapContainer className={mapClassName} {...rest}>
