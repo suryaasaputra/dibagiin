f9465db1661bcecbd80b14f6adb8244b529856ec
suryaasaputra <suryaaasaputra.s@gmail.com>
2022-12-09 10:28:54 +0700

    ubah pwa

--- next.config.js
+++ next.config.js
@@ -1,11 +1,31 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development"
-});
-const nextConfig = withPWA({
+const withPWA = require('next-pwa');
+// const withPWA = require("next-pwa")({
+//   dest: "public",
+//   register: true,
+//   skipWaiting: true,
+//   disable: process.env.NODE_ENV === "development"
+// });
+// const nextConfig = withPWA({
+//   reactStrictMode: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'storage.googleapis.com',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+// })
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    skipWaiting: true,
+  },
   reactStrictMode: true,
   images: {
     remotePatterns: [
@@ -16,6 +36,4 @@ const nextConfig = withPWA({
       },
     ],
   },
-})
-
-module.exports = nextConfig
+});
