c13b37edbfcf9104dc81b64ae5cd731bae5c47fb
suryaasaputra <suryaaasaputra.s@gmail.com>
2023-01-21 00:32:49 +0700

    ubah base yrl ke vps

--- globals/config.js
+++ globals/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-  BASE_URL: 'https://backend-dibagiin-edmebdkc2a-et.a.run.app',
-  // BASE_URL: 'http://104.248.153.63/',
+  // BASE_URL: 'https://backend-dibagiin-edmebdkc2a-et.a.run.app',
+  BASE_URL: 'https://104.248.153.63',
 };
 
 export default CONFIG;
