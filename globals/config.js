7f0bf0e0ed323d5d61bcbba773ed620ee6cb249e
suryaasaputra <suryaaasaputra.s@gmail.com>
2023-01-22 16:46:39 +0700

    update base url

--- globals/config.js
+++ globals/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   // BASE_URL: 'https://backend-dibagiin-edmebdkc2a-et.a.run.app',
-  BASE_URL: 'https://165.232.175.90/',
+  BASE_URL: 'https://165.232.175.90',
 };
 
 export default CONFIG;
